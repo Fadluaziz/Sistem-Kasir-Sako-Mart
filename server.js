527d5504641c46f6e7ab9d27df08da701b93b3b6
Fadhlu <fadhluaziz2104@gmail.com>
2026-02-07 15:33:42 +0700

    database logic and scheme finalized

--- server.js
+++ server.js
@@ -133,4 +133,8 @@ app.get('/api/stats', (req, res) => {
 });
 
 // JALANKAN SERVER
-app.listen(3000, () => console.log('Server ON: http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Server running on port ' + PORT);
+});
